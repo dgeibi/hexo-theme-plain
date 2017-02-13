b20f49d68f7896039bc841230fbdac024d8ad138
dgeibi <png.inside@gmail.com>
2017-02-13 16:22:20 +0800

    alter

--- source/sw.js
+++ source/sw.js
@@ -1,8 +1,8 @@
 // vecka.14islands.com/service-worker.js
 // https://github.com/14islands/vecka.14islands.com/blob/master/server/service-worker.js
 
-const ASSETS_CACHE = "assets-v3.2"
-const PAGES_CACHE = "pages-v1"
+const ASSETS_CACHE = "assets-v3.3"
+const PAGES_CACHE = "pages-v1.1"
 const expectedCaches = [ASSETS_CACHE, PAGES_CACHE]
 const urlsToCache = [
   '/css/style.css',
